a5b534c17a6ee6eb37cfe4f5ac2f7212093030a6
chernikov <chernikov@gmail.com>
2016-01-16 21:37:47 +0200

    Bootstrap lesson

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿(function (){
+    var ele = $("#username");
+    ele.text("Sam Jones");
+
+    var main = $("#main");
+
+    main.on("mouseenter", function () {
+        main.style = "background-color : #888;";
+    });
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //})
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    var $icon = $("#sidebarToggle i.fa");
+
+    $("#sidebarToggle").click(function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
+        } else {
+            $icon.removeClass("fa-angle-right");
+            $icon.addClass("fa-angle-left");
+            
+        }
+    });
+})();
